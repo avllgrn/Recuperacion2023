ab0372857cfb8e1e98e577ab71280c43ed90f528
Alfredo Villagran <avillagran@ipn.mx>
2023-04-26 15:01:25 -0500

    Mostrar operandos y operadores

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 int main(void){
-    printf("%d\n",20+13);
-    printf("%d\n",20-13);
-    printf("%d\n",20*13);
-    printf("%d\n",20/13);
-    printf("%d\n\n",20%13);
+    printf("%d + %d = %d\n",2,3,2+3);
+    printf("%d - %d = %d\n",2,3,2-3);
+    printf("%d * %d = %d\n",2,3,2*3);
+    printf("%d / %d = %d\n",2,3,2/3);
+    printf("%d %% %d = %d\n\n",2,3,2%3);
 
-    printf("%f\n",20.1+13);
-    printf("%f\n",20.2-13);
-    printf("%f\n",20.3*13);
-    printf("%f\n",20.4/13);
+    printf("%f + %f = %f\n",2.5,3.5,2.5+3.5);
+    printf("%f - %f = %f\n",2.5,3.5,2.5-3.5);
+    printf("%f * %f = %f\n",2.5,3.5,2.5*3.5);
+    printf("%f / %f = %f\n\n",2.5,3.5,2.5/3.5);
 
     return 0;
 }
