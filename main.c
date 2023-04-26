67021496248848b51e22381404983558602aaad2
Alfredo Villagran <avillagran@ipn.mx>
2023-04-26 15:01:06 -0500

    Mostrar resultado de operacion

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 
 int main(void){
-    printf("Hello world!\n");
+    printf("%d\n",20+13);
+    printf("%d\n",20-13);
+    printf("%d\n",20*13);
+    printf("%d\n",20/13);
+    printf("%d\n\n",20%13);
+
+    printf("%f\n",20.1+13);
+    printf("%f\n",20.2-13);
+    printf("%f\n",20.3*13);
+    printf("%f\n",20.4/13);
+
     return 0;
 }
